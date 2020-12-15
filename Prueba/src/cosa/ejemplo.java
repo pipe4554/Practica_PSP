6b2aeb5d98387a5edffa549e4ee9118efa8e2f86
pipe4554 <0273lmejia@e-itaca.es>
2020-12-15 20:50:54 +0100

    ejercicio de prueba

---
+++ Prueba/src/cosa/ejemplo.java
@@ -0,0 +1,22 @@
+package cosa;
+
+
+import java.util.ArrayList;
+
+
+public class ejemplo {
+
+	public static void main(String[] args) {
+		int num = 0;
+
+		for (int i = 0; i < 10; i++) {
+			num++;
+		}
+		System.out.println(num);
+
+		ArrayList<String> numeros = new ArrayList<String>();
+	}
+
+	
+
+}
---
+++ Prueba/src/cosa/producto.java
@@ -0,0 +1,22 @@
+package cosa;
+
+public class producto {
+	public String Nombre;
+	public float precio;
+	
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
+}
